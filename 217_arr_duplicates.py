ff366d4d84a670d16c3ad86a61696df6b08be403
euph0x1 <ahmedfaria99@gmail.com>
2026-04-19 17:42:44 +0530

    arr dup

--- 217_arr_duplicates.py
+++ 217_arr_duplicates.py
@@ -19,3 +19,24 @@ class Solution:
                 if nums[i] == nums[j]:
                     return True
         return False
+
+#o(nlogn) space o( 1 )
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        l,r = 0,1
+        while r < len(nums):
+            if nums[l] == nums[r]:
+                return True
+            l += 1
+            r += 1
+        return False
+    
+#just check adjacent 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
+
